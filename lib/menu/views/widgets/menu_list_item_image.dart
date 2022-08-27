1f25e6ece507267e9756eaa30f47cd4cf065968d
roaa94 <roaa94khaddam@gmail.com>
2022-08-27 18:15:53 +0300

    chore: removes gyroscope effect code

--- lib/menu/views/widgets/menu_list_item_image.dart
+++ lib/menu/views/widgets/menu_list_item_image.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vikings/menu/models/food_menu_item.dart';
-import 'package:flutter_vikings/menu/views/widgets/menu_list_item_image_bg.dart';
 import 'package:flutter_vikings/menu/views/widgets/menu_list_item_image_wrapper.dart';
 
 class MenuListItemImage extends StatelessWidget {
@@ -14,25 +13,22 @@ class MenuListItemImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double imageSize = MediaQuery.of(context).size.width * 0.45;
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(35),
-      child: MenuListItemImageWrapper(
-        child: Stack(
-          children: [
-            MenuListItemImageBg(
-              imageSize: imageSize,
-              color: menuItem.textColor.withOpacity(0.2),
-            ),
-            Positioned(
-              bottom: -20,
-              right: -20,
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: -20,
+            right: -20,
+            child: MenuListItemImageWrapper(
               child: Image.asset(
                 menuItem.image,
                 width: imageSize,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
