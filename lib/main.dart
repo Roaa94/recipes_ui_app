614ff10291228bf2643fcd7d0344ab93654d7f7c
roaa94 <roaa94khaddam@gmail.com>
2022-08-27 21:33:36 +0300

    chore: precaches menu images

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_vikings/core/styles/app_themes.dart';
+import 'package:flutter_vikings/menu/food_menu_data.dart';
+import 'package:flutter_vikings/menu/models/food_menu_item.dart';
 import 'package:flutter_vikings/menu/views/pages/food_menu_page.dart';
 
 void main() {
@@ -12,8 +14,26 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isInit = true;
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      for (FoodMenuItem menuItem in FoodMenuData.dessertMenu) {
+        precacheImage(Image.asset(menuItem.image).image, context);
+      }
+    }
+    _isInit = false;
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
