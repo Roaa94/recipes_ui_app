6a694b27801542f7a27c50e64f1c0c469133e1be
roaa94 <roaa94khaddam@gmail.com>
2022-08-28 12:42:37 +0300

    feat: implements shadow gyroscope effect to recipe image pattern

--- lib/features/recipes/views/widgets/recipe_image_pattern_mobile.dart
+++ lib/features/recipes/views/widgets/recipe_image_pattern_mobile.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_vikings/features/recipes/models/recipe.dart';
@@ -15,45 +17,88 @@ class RecipeImagePatternMobile extends StatefulWidget {
   final BorderRadius borderRadius;
 
   @override
-  State<RecipeImagePatternMobile> createState() => _RecipeImagePatternMobileState();
+  State<RecipeImagePatternMobile> createState() =>
+      _RecipeImagePatternMobileState();
 }
 
 class _RecipeImagePatternMobileState extends State<RecipeImagePatternMobile> {
   double x = 0;
   double y = 0;
+  static const double maxMovableDistance = 10;
 
   @override
   Widget build(BuildContext context) {
-    return Consumer(
-      child: ClipRRect(
-        borderRadius: widget.borderRadius,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(widget.recipe.bgImage),
-              alignment: Alignment.bottomCenter,
+    return Stack(
+      children: [
+        Consumer(
+          child: ClipRRect(
+            borderRadius: widget.borderRadius,
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaY: 3, sigmaX: 3),
+              child: Image.asset(
+                widget.recipe.bgImage,
+                color: Colors.black.withOpacity(0.3),
+                alignment: Alignment.bottomCenter,
+              ),
             ),
           ),
-        ),
-      ),
-      builder: (context, ref, child) {
-        final GyroscopeEvent? gyroscopeEvent =
-            ref.watch(gyroscopeProvider).value;
-        if (gyroscopeEvent != null) {
-          y += gyroscopeEvent.y;
-          x += gyroscopeEvent.x;
-        }
+          builder: (context, ref, child) {
+            final GyroscopeEvent? gyroscopeEvent =
+                ref.watch(gyroscopeProvider).value;
+            if (gyroscopeEvent != null) {
+              y += gyroscopeEvent.y;
+              x += gyroscopeEvent.x;
+            }
 
-        return AnimatedPositioned(
-          top: x.clamp(-10, 10),
-          bottom: -(x.clamp(-10, 10)).toDouble(),
-          left: y.clamp(-10, 10),
-          right: -(y.clamp(-10, 10)).toDouble(),
-          duration: const Duration(milliseconds: 100),
-          curve: Curves.easeOut,
-          child: child!,
-        );
-      },
+            return AnimatedPositioned(
+              top: x.clamp(-maxMovableDistance, maxMovableDistance) * 1.5,
+              bottom: -(x.clamp(-maxMovableDistance, maxMovableDistance))
+                      .toDouble() *
+                  1.5,
+              left: y.clamp(-maxMovableDistance, maxMovableDistance) * 1.5,
+              right: -(y.clamp(-maxMovableDistance, maxMovableDistance))
+                      .toDouble() *
+                  1.5,
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeOut,
+              child: child!,
+            );
+          },
+        ),
+        Consumer(
+          child: ClipRRect(
+            borderRadius: widget.borderRadius,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(widget.recipe.bgImage),
+                  alignment: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+          ),
+          builder: (context, ref, child) {
+            final GyroscopeEvent? gyroscopeEvent =
+                ref.watch(gyroscopeProvider).value;
+            if (gyroscopeEvent != null) {
+              y += gyroscopeEvent.y;
+              x += gyroscopeEvent.x;
+            }
+
+            return AnimatedPositioned(
+              top: x.clamp(-maxMovableDistance, maxMovableDistance),
+              bottom: -(x.clamp(-maxMovableDistance, maxMovableDistance))
+                  .toDouble(),
+              left: y.clamp(-maxMovableDistance, maxMovableDistance),
+              right: -(y.clamp(-maxMovableDistance, maxMovableDistance))
+                  .toDouble(),
+              duration: const Duration(milliseconds: 100),
+              curve: Curves.easeOut,
+              child: child!,
+            );
+          },
+        ),
+      ],
     );
   }
 }
