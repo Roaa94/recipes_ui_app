c870f76d0306291ee61e43a075af02c7aa6831d8
roaa94 <roaa94khaddam@gmail.com>
2022-08-29 02:16:38 +0300

    fix: removes unused import

--- lib/features/recipes/views/widgets/recipe_image_pattern_mouse.dart
+++ lib/features/recipes/views/widgets/recipe_image_pattern_mouse.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_vikings/core/enums/screen_size.dart';
 import 'package:flutter_vikings/core/styles/app_colors.dart';
